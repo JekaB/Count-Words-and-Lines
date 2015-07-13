76e8aea56db5be862d404fe49a1ec7b09d156c68
Evgeiy_B <bocharovevgen@gmail.com>
2015-07-13 18:23:35 +0300

    new origin commit

---
+++ Count Words and Lines/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Count_Words_and_Lines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string text = System.IO.File.ReadAllText(@"C:\Users\Jeka\Desktop\S&W.txt");
+                List<string> words = text.Split(' ').ToList<string>();
+                int lines = System.IO.File.ReadAllLines(@"C:\Users\Jeka\Desktop\S&W.txt").Length;
+                Console.WriteLine("Слов в тексте {0}\nСтрок в тексте {1}", words.Count, lines);
+
+
+
+
+                
+                Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                Console.ReadKey();
+            }
+
+            
+            
+        }
+    }
+}
